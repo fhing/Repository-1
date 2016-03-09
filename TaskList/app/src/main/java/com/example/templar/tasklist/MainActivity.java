f13b3c0b814ee18405a7cd2ad7e18e786e46a11a
Florent Hing <florenthing@yahoo.fr>
2016-03-09 11:47:46 +0000

    Milestone 1

--- TaskList/app/src/main/java/com/example/templar/tasklist/MainActivity.java
+++ TaskList/app/src/main/java/com/example/templar/tasklist/MainActivity.java
@@ -1,23 +1,27 @@
 package com.example.templar.tasklist;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements View.OnClickListener{
-    private Button btn_add;
-    private TextView tv_msg;
-    private EditText et_box;
+import java.util.ArrayList;
 
-    @Override
+public class MainActivity extends Activity implements View.OnKeyListener{
+
+
+    //@Override
     public boolean onKey(View v, int keyCode, KeyEvent event) {
-
         // if we get a key down event on the enter button change the text on the textview
         if (v.getId() == et_box.getId() &&
                 event.getAction() == KeyEvent.ACTION_DOWN &&
@@ -33,19 +37,34 @@ public class MainActivity extends Activity implements View.OnClickListener{
         if(v.getId() == btn_add.getId())
             tv_msg.setText("Task added! ");
     }
+
+    private Button btn_add;
+    private TextView tv_msg;
+    private EditText et_box;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         btn_add = (Button) findViewById(R.id.btn_add);
-
+        //btn_add.setOnClickListener(this);
         et_box = (EditText) findViewById(R.id.et_box);
-
         tv_msg = (TextView) findViewById(R.id.tv_msg);
 
         et_box.setOnKeyListener(this);
-        btn_add.setOnClickListener(this);
 
+        ListView lv_mainlist = (ListView) findViewById(R.id.lv_mainlist);
+        ArrayList<String> al_strings = new ArrayList<String>();
+        ArrayAdapter<String> aa = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, al_strings);
+// make the list view an observer of the array adapter.
+        lv_mainlist.setAdapter(aa);
+// add in 3 strings to the array adapter and ask the list view to update itself
+        al_strings.add("this is the first string");
+        al_strings.add("this is the second string");
+        al_strings.add("this is the third string");
+        aa.notifyDataSetChanged();
         //tv_msg.setText(Click);
     }
 
